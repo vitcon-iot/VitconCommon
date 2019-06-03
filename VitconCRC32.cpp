16dfcb4e5af94b9b33566a47040a59d418f01fd5
Vitcon <master@vitcon.co.kr>
2019-06-03 16:04:21 +0900

    Update VitconCRC32.cpp

--- VitconCRC32.cpp
+++ VitconCRC32.cpp
@@ -1,15 +1,21 @@
 /**
  * Created by Park, Nil on 2017-03-09.
- * Last Revision: 2017-04-26
+ * Last Revision: 2019-06-03
  *
  * VitconIOTItem.h의 Description 참조
  */
 
 #include "VitconCRC32.h"
+#ifdef __AVR__
 #include <avr/pgmspace.h>
+#endif
 
 // polynomial: 0xEDB88320
+#ifdef __AVR__
 static const uint32_t crc_table[] PROGMEM = {
+#else
+static const uint32_t crc_table[] = {
+#endif
   0x00000000, 0x77073096, 0xEE0E612C, 0x990951BA, 0x076DC419, 0x706AF48F, 0xE963A535, 0x9E6495A3, 
   0x0EDB8832, 0x79DCB8A4, 0xE0D5E91E, 0x97D2D988, 0x09B64C2B, 0x7EB17CBD, 0xE7B82D07, 0x90BF1D91, 
   0x1DB71064, 0x6AB020F2, 0xF3B97148, 0x84BE41DE, 0x1ADAD47D, 0x6DDDE4EB, 0xF4D4B551, 0x83D385C7, 
@@ -64,7 +70,11 @@ void CRC32::Reset()
 void CRC32::Update(uint8_t b)
 {
   mCrc = ~mCrc;
+#ifdef __AVR__
   mCrc = pgm_read_dword_near(&crc_table[(mCrc ^ b) & 0xFF]) ^ (mCrc >> 8);
+#else
+  mCrc = crc_table[(mCrc ^ b) & 0xFF] ^ (mCrc >> 8);
+#endif
   mCrc = ~mCrc;
 }
 
@@ -73,7 +83,11 @@ void CRC32::Update(const uint8_t *bytes, int len)
   mCrc = ~mCrc;
   for (int i = 0; i < len; i++)
   {
+#ifdef __AVR__
     mCrc = pgm_read_dword_near(&crc_table[(mCrc ^ bytes[i]) & 0xFF]) ^ (mCrc >> 8);
+#else
+  mCrc = crc_table[(mCrc ^ bytes[i]) & 0xFF] ^ (mCrc >> 8);
+#endif
   }
   mCrc = ~mCrc;
 }
